0ffac25b06b661dfd694a5571788143dccdb8a89
iMorreeu <70594197+iMorreeu@users.noreply.github.com>
2020-10-19 23:53:19 -0300

    RandomCH

---
+++ Challenges/ch2.js
@@ -0,0 +1,18 @@
+// Write a function that takes arguments an arbitrary number of arrays
+// It should return an array containing the values of all arrays
+//
+const arr1 = [1,2,3]
+const arr2 = [4,2,'3']
+const arr3 = [5,'3',4]
+const arr4 = [false, false, false]
+
+function myFunction(arr1,arr2,arr3,arr4) {
+    all = arr1.concat(arr2,arr3,arr4)
+    return console.log(all)
+
+
+    
+
+    }
+    myFunction(arr1,arr2,arr3,arr4)
+
---
+++ Challenges/ch3.js
@@ -0,0 +1,14 @@
+// This challenge is a little bit more complex
+// Write a function that takes a number as argument
+// If the number is prime, return the number
+// If not, return the next higher prime number
+//
+var n = prompt()
+var n2 = prompt()
+function myFunction(n1,n2) {
+    
+    return console.log(n1 === n2)
+ 
+
+}
+myFunction(n, n2)
